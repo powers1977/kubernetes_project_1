e92ea681d8cff62ffeb4471fbed3dbe259d576e9
Ryan Powers <ryan.p.powers@gmail.com>
2025-04-26 13:53:27 -0500

    scripts for docker

---
+++ build_and_push.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Define variables
+IMAGE_NAME="my-nginx"       # Image name
+ACR_NAME="acrrealmink"      # Azure Container Registry name
+VERSION_TAG="v1.0"          # Version tag for your image
+LATEST_TAG="latest"         # Tag for latest
+
+# Build the Docker image
+echo "Building Docker image..."
+docker build -t $IMAGE_NAME .
+
+# Tag the image with both version and latest tags
+echo "Tagging image with version and latest tags..."
+docker tag $IMAGE_NAME:latest $ACR_NAME.azurecr.io/$IMAGE_NAME:$VERSION_TAG
+docker tag $IMAGE_NAME:latest $ACR_NAME.azurecr.io/$IMAGE_NAME:$LATEST_TAG
+
+# Log in to Azure ACR
+echo "Logging into Azure ACR..."
+az acr login --name $ACR_NAME
+
+# Push the image to Azure ACR
+echo "Pushing image to Azure ACR..."
+docker push $ACR_NAME.azurecr.io/$IMAGE_NAME:$VERSION_TAG
+docker push $ACR_NAME.azurecr.io/$IMAGE_NAME:$LATEST_TAG
+
+echo "Done!"

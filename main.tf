efef01019245928a669c3fd4c4d7932ba2204010
Ryan Powers <ryan.p.powers@gmail.com>
2025-04-27 12:19:39 -0500

    update principal_id setting

--- main.tf
+++ main.tf
@@ -81,7 +81,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
 resource "azurerm_role_assignment" "aks_acr_pull" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  #principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 }
 
 # Output ACR Login Server URL

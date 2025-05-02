0844c23e2765e0acf2592adf6bc4817f4741fd5d
Ryan Powers <ryan.p.powers@gmail.com>
2025-05-02 11:06:52 -0500

    Few changes regarding the tag

--- scripts/build_and_push_nginx.sh
+++ scripts/build_and_push_nginx.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Define variables
+WORKING_DIR=$(pwd)/..
 IMAGE_NAME="my-nginx"       # Image name
 ACR_NAME="acrgrownpossum"      # Azure Container Registry name
 VERSION_TAG="v1.0"          # Version tag for your image
@@ -8,12 +9,12 @@ LATEST_TAG="latest"         # Tag for latest
 
 # Build the Docker image
 echo "Building Docker image..."
-docker build -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME:$LATEST_TAG -f $WORKING_DIR/nginx/Dockerfile $WORKING_DIR/nginx
 
 # Tag the image with both version and latest tags
 echo "Tagging image with version and latest tags..."
-docker tag $IMAGE_NAME:latest $ACR_NAME.azurecr.io/$IMAGE_NAME:$VERSION_TAG
-docker tag $IMAGE_NAME:latest $ACR_NAME.azurecr.io/$IMAGE_NAME:$LATEST_TAG
+docker tag $IMAGE_NAME:$LATEST_TAG $ACR_NAME.azurecr.io/$IMAGE_NAME:$VERSION_TAG
+docker tag $IMAGE_NAME:$LATEST_TAG $ACR_NAME.azurecr.io/$IMAGE_NAME:$LATEST_TAG
 
 # Log in to Azure ACR
 echo "Logging into Azure ACR..."
@@ -25,3 +26,4 @@ docker push $ACR_NAME.azurecr.io/$IMAGE_NAME:$VERSION_TAG
 docker push $ACR_NAME.azurecr.io/$IMAGE_NAME:$LATEST_TAG
 
 echo "Done!"
+

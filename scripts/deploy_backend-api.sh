d3ec889f75664b9c97a49bc5ca78f3cc9659c2cc
Ryan Powers <ryan.p.powers@gmail.com>
2025-05-05 17:10:23 -0500

    Fixing some file names

--- scripts/deploy_backend-api.sh
+++ scripts/deploy_backend-api.sh
@@ -5,18 +5,18 @@ WORKING_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 K8S_DIR="$WORKING_DIR/backend-api"
 
 #THIS WILL DEPLOY AND START THE POD
-printf "Deploying Nginx to AKS:  kubectl apply -f ${K8S_DIR}/app-deployment.yaml \n"
-kubectl apply -f ${K8S_DIR}/app-deployment.yaml
+printf "Deploying Nginx to AKS:  kubectl apply -f ${K8S_DIR}/k8s/backend-api-deployment.yaml \n"
+kubectl apply -f ${K8S_DIR}/k8s/backend-api-deployment.yaml
 
 printf "Continue? \n"
 read ANSWER
 
 #THIS WILL DEPLOY THE SERVICE
-printf "Deploying the service: kubectl apply -f ${K8S_DIR}/backend-api-service.yaml \n"
-kubectl apply -f ${K8S_DIR}/backend-api-service.yaml
+printf "Deploying the service: kubectl apply -f ${K8S_DIR}/k8s/backend-api-service.yaml \n"
+kubectl apply -f ${K8S_DIR}/k8s/backend-api-service.yaml
 
 #COMMAND TO DO BOTH AT SAME TIME:
-#kubectl apply -f "$K8S_DIR/app-deployment.yaml" -f "$K8S_DIR/backend-api-service.yaml"
+#kubectl apply -f "$K8S_DIR/k8s/backend-api-deployment.yaml" -f "$K8S_DIR/k8s/backend-api-service.yaml"
 
 echo "Back-end deployment applied!"
 

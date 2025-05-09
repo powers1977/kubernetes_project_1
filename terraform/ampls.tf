2b42c94350c8d148d84fcdd7cd8e5af857dceb2b
Ryan Powers <ryan.p.powers@gmail.com>
2025-05-09 13:54:41 -0500

    removing location

--- terraform/ampls.tf
+++ terraform/ampls.tf
@@ -1,6 +1,5 @@
 resource "azurerm_monitor_private_link_scope" "ampls" {
   name                = "${var.project_name}-ampls"
-  location            = azurerm_resource_group.container_rg.location
   resource_group_name = azurerm_resource_group.container_rg.name
 }
 

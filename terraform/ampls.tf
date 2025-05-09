e25ca64c5dd77cc64c24ab7a6511984ca15770f8
Ryan Powers <ryan.p.powers@gmail.com>
2025-05-09 14:48:08 -0500

    troubleshooting ampls connection

--- terraform/ampls.tf
+++ terraform/ampls.tf
@@ -7,6 +7,6 @@ resource "azurerm_monitor_private_link_scoped_service" "ampls_law" {
   name                 = "ampls-law"
   resource_group_name  = azurerm_monitor_private_link_scope.ampls.resource_group_name
   scope_name           = azurerm_monitor_private_link_scope.ampls.name
-  linked_resource_id   = azurerm_log_analytics_workspace.law.id
+  linked_resource_id   = azurerm_log_analytics_workspace.monitoring_law.id 
 }
 

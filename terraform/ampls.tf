4c26a4268b8622feebdfd7455709b5599bd66af5
Ryan Powers <ryan.p.powers@gmail.com>
2025-05-09 14:19:48 -0500

    Commenting out a section

--- terraform/ampls.tf
+++ terraform/ampls.tf
@@ -3,15 +3,15 @@ resource "azurerm_monitor_private_link_scope" "ampls" {
   resource_group_name = azurerm_resource_group.container_rg.name
 }
 
-resource "azurerm_monitor_private_link_scope_log_analytics_workspace_association" "ampls_law" {
-  scope_name          = azurerm_monitor_private_link_scope.ampls.name
-  resource_group_name = azurerm_resource_group.container_rg.name
-  workspace_id        = azurerm_log_analytics_workspace.monitoring_law.id
-}
-
-resource "azurerm_monitor_private_link_scope_data_collection_endpoint_association" "ampls_dce" {
-  scope_name              = azurerm_monitor_private_link_scope.ampls.name
-  resource_group_name     = azurerm_resource_group.container_rg.name
-  data_collection_endpoint_id = azurerm_monitor_data_collection_endpoint.aks_dce.id
-}
+#resource "azurerm_monitor_private_link_scope_log_analytics_workspace_association" "ampls_law" {
+#  scope_name          = azurerm_monitor_private_link_scope.ampls.name
+#  resource_group_name = azurerm_resource_group.container_rg.name
+#  workspace_id        = azurerm_log_analytics_workspace.monitoring_law.id
+#}
+#
+#resource "azurerm_monitor_private_link_scope_data_collection_endpoint_association" "ampls_dce" {
+#  scope_name              = azurerm_monitor_private_link_scope.ampls.name
+#  resource_group_name     = azurerm_resource_group.container_rg.name
+#  data_collection_endpoint_id = azurerm_monitor_data_collection_endpoint.aks_dce.id
+#}
 

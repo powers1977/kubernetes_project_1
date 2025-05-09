32f6101e54af25f4ae1bb47fa94e721b6e2058bf
Ryan Powers <ryan.p.powers@gmail.com>
2025-05-09 09:18:58 -0500

    fixing typo

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,7 +63,7 @@ resource "azurerm_virtual_network" "aks_vnet" {
   name                = "${var.project_name}-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = var.location
-  resource_group_name = aurerm_resource_group.container_rg.name 
+  resource_group_name = azurerm_resource_group.container_rg.name 
   #tags                = var.tags
 }
 

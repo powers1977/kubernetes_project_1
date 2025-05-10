69521454a8d5fa24c5eb27d614b7678342abf23d
Ryan Powers <ryan.p.powers@gmail.com>
2025-05-10 11:26:46 -0500

    commenting out PE

--- terraform/ampls.tf
+++ terraform/ampls.tf
@@ -10,17 +10,17 @@ resource "azurerm_monitor_private_link_scoped_service" "ampls_law" {
   linked_resource_id   = azurerm_log_analytics_workspace.monitoring_law.id 
 }
 
-resource "azurerm_private_endpoint" "ampls_pe" {
-  name                = "${var.project_name}-ampls-pe"
-  location            = azurerm_resource_group.container_rg.location
-  resource_group_name = azurerm_resource_group.container_rg.name
-  subnet_id           = azurerm_subnet.private_link_subnet.id
-
-  private_service_connection {
-    name                           = "${var.project_name}-ampls-psc"
-    private_connection_resource_id = azurerm_monitor_private_link_scope.ampls.id
-    is_manual_connection           = false
-    subresource_names              = ["azuremonitor"]
-  }
-}
+#resource "azurerm_private_endpoint" "ampls_pe" {
+#  name                = "${var.project_name}-ampls-pe"
+#  location            = azurerm_resource_group.container_rg.location
+#  resource_group_name = azurerm_resource_group.container_rg.name
+#  subnet_id           = azurerm_subnet.private_link_subnet.id
+#
+#  private_service_connection {
+#    name                           = "${var.project_name}-ampls-psc"
+#    private_connection_resource_id = azurerm_monitor_private_link_scope.ampls.id
+#    is_manual_connection           = false
+#    subresource_names              = ["azuremonitor"]
+#  }
+#}
 

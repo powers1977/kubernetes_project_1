cfa9767c89eede24de120ceab835ccb68f217d05
Ryan Powers <ryan.p.powers@gmail.com>
2025-05-09 19:57:42 -0500

    another DNS zone

--- terraform/private_dns.tf
+++ terraform/private_dns.tf
@@ -10,3 +10,14 @@ resource "azurerm_private_dns_zone_virtual_network_link" "dns_link" {
   virtual_network_id    = azurerm_virtual_network.aks_vnet.id
 }
 
+resource "azurerm_private_dns_zone" "oms_opinsights" {
+  name                = "oms.opinsights.azure.com"
+  resource_group_name = "grownpossum-rg"
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "oms_opinsights_dns_link" {
+  name                  = "oms-opinsights-dns-link"  # Unique name
+  resource_group_name   = "grownpossum-rg"
+  private_dns_zone_name = azurerm_private_dns_zone.oms_opinsights.name  # Correct reference
+  virtual_network_id    = azurerm_virtual_network.aks_vnet.id
+}

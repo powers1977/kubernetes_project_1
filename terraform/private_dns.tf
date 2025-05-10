ccb3c4beed135891931bcc99a4f8f9acd9f474f3
Ryan Powers <ryan.p.powers@gmail.com>
2025-05-10 07:46:15 -0500

    update file structure

--- terraform/private_dns.tf
+++ terraform/private_dns.tf
@@ -1,23 +1,23 @@
-resource "azurerm_private_dns_zone" "ampls_dns" {
-  name                = "privatelink.monitor.azure.com"
+variable "private_dns_zones" {
+  default = {
+    "ampls"        = "privatelink.monitor.azure.com"
+    "oms"          = "privatelink.oms.opinsights.azure.com"
+    "ods"          = "privatelink.ods.opinsights.azure.com"
+    "aks"          = "privatelink.azmk8s.io"
+  }
+}
+
+resource "azurerm_private_dns_zone" "zones" {
+  for_each            = var.private_dns_zones
+  name                = each.value
   resource_group_name = azurerm_resource_group.container_rg.name
 }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "dns_link" {
-  name                  = "ampls-dns-link"
-  resource_group_name   = azurerm_resource_group.container_rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.ampls_dns.name
-  virtual_network_id    = azurerm_virtual_network.aks_vnet.id
+resource "azurerm_private_dns_zone_virtual_network_link" "links" {
+  for_each                = var.private_dns_zones
+  name                    = "${each.key}-dns-link"
+  resource_group_name     = azurerm_resource_group.container_rg.name
+  private_dns_zone_name   = azurerm_private_dns_zone.zones[each.key].name
+  virtual_network_id      = azurerm_virtual_network.aks_vnet.id
 }
 
-resource "azurerm_private_dns_zone" "oms_opinsights" {
-  name                = "oms.opinsights.azure.com"
-  resource_group_name = "grownpossum-rg"
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "oms_opinsights_dns_link" {
-  name                  = "oms-opinsights-dns-link"  # Unique name
-  resource_group_name   = "grownpossum-rg"
-  private_dns_zone_name = azurerm_private_dns_zone.oms_opinsights.name  # Correct reference
-  virtual_network_id    = azurerm_virtual_network.aks_vnet.id
-}

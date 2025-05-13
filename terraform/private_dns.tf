2a09d3519f81a727b8d844e8513bba8acb2f7207
Ryan Powers <ryan.p.powers@gmail.com>
2025-05-13 15:49:09 -0500

    Commenting out for now

--- terraform/private_dns.tf
+++ terraform/private_dns.tf
@@ -1,23 +1,25 @@
-variable "private_dns_zones" {
-  default = {
-    "ampls"        = "privatelink.monitor.azure.com"
-    "oms"          = "privatelink.oms.opinsights.azure.com"
-    "ods"          = "privatelink.ods.opinsights.azure.com"
-    "aks"          = "privatelink.azmk8s.io"
-  }
-}
 
-resource "azurerm_private_dns_zone" "zones" {
-  for_each            = var.private_dns_zones
-  name                = each.value
-  resource_group_name = azurerm_resource_group.container_rg.name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "links" {
-  for_each                = var.private_dns_zones
-  name                    = "${each.key}-dns-link"
-  resource_group_name     = azurerm_resource_group.container_rg.name
-  private_dns_zone_name   = azurerm_private_dns_zone.zones[each.key].name
-  virtual_network_id      = azurerm_virtual_network.aks_vnet.id
-}
+##_Leaving this out for now.  
+#variable "private_dns_zones" {
+#  default = {
+#    "ampls"        = "privatelink.monitor.azure.com"
+#    "oms"          = "privatelink.oms.opinsights.azure.com"
+#    "ods"          = "privatelink.ods.opinsights.azure.com"
+#    "aks"          = "privatelink.azmk8s.io"
+#  }
+#}
+#
+#resource "azurerm_private_dns_zone" "zones" {
+#  for_each            = var.private_dns_zones
+#  name                = each.value
+#  resource_group_name = azurerm_resource_group.container_rg.name
+#}
+#
+#resource "azurerm_private_dns_zone_virtual_network_link" "links" {
+#  for_each                = var.private_dns_zones
+#  name                    = "${each.key}-dns-link"
+#  resource_group_name     = azurerm_resource_group.container_rg.name
+#  private_dns_zone_name   = azurerm_private_dns_zone.zones[each.key].name
+#  virtual_network_id      = azurerm_virtual_network.aks_vnet.id
+#}
 

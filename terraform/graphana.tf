ba1119fa2a311afe2520c3e2a7dafa675fe6e2e0
Ryan Powers <ryan.p.powers@gmail.com>
2025-05-16 15:12:43 -0500

    Adding graphana version

--- terraform/graphana.tf
+++ terraform/graphana.tf
@@ -2,6 +2,7 @@ resource "azurerm_dashboard_grafana" "main" {
   name                = "${var.project_name}-grafana"
   location            = var.location
   resource_group_name = azurerm_resource_group.container_rg.name
+  grafana_major_version  = 9
 
   identity {
     type = "SystemAssigned"
